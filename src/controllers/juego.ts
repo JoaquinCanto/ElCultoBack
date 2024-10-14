08332ec683ad9bb2da11926963d1accfd34e9fd2
Joaquin Fernan Canto <joaquinfr.canto@gmail.com>
2024-10-14 11:52:44 -0300

    Added Juego routes and controller.

---
+++ src/controllers/juego.ts
@@ -0,0 +1,109 @@
+import { Request, Response } from "express";
+import { PrismaClient, Juego } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const juegoController = {
+	// Obtener todos los juegos
+	getAll: async (_req: Request, res: Response) => {
+		try {
+			const alljuegos: Juego[] = await prisma.juego.findMany();
+			return res.status(200).json({
+				status: 200,
+				total: alljuegos.length,
+				data: alljuegos,
+			});
+		} catch (error) {
+			if (error instanceof Error) {
+				return res.status(204).json({
+					message: error.message,
+					error: true,
+				});
+			}
+		}
+	},
+
+	//Obtener un juego por id
+	getById: async (_req: Request, res: Response) => {
+		try {
+			const id = parseInt(_req.params.id);
+			const idJuego: Juego | null = await prisma.juego.findUnique({ where: { idJuego: Number(id) } });
+			return res.status(200).json({
+				status: 200,
+				data: idJuego,
+			});
+		} catch (error) {
+			if (error instanceof Error) {
+				return res.status(204).json({
+					message: error.message,
+					error: true,
+				});
+			}
+		}
+	},
+
+	//Crear un juego
+	create: async (req: Request, res: Response) => {
+		try {
+			const newJuego: Juego = await prisma.juego.create({
+				data: {
+					...req.body
+				}
+			});
+			res.status(201).json({
+				message: 'Juego creado exitosamente.',
+				status: 201,
+				data: newJuego,
+				error: false,
+			});
+		} catch (error) {
+			if (error instanceof Error) {
+				return res.status(400).json({
+					message: error.message,
+					error: true,
+				});
+			}
+		}
+	},
+
+	//Borrar un juego por id
+	delete: async (_req: Request, res: Response) => {
+		try {
+			const id = parseInt(_req.params.id);
+			const deleteJuego = await prisma.juego.delete({ where: { idJuego: Number(id) } });
+			return res.status(200).json({
+				status: 200,
+				data: deleteJuego,
+			});
+		} catch (error) {
+			if (error instanceof Error) {
+				return res.status(204).json({
+					message: error.message,
+					error: true,
+				});
+			}
+		}
+	},
+
+	//Modificar un juego por id
+	update: async (_req: Request, res: Response) => {
+		try {
+			const id = parseInt(_req.params.id);
+			const data = _req.body;
+			const updateJuego: Juego = await prisma.juego.update({ where: { idJuego: Number(id) }, data: data });
+			return res.status(200).json({
+				status: 200,
+				data: updateJuego,
+			});
+		} catch (error) {
+			if (error instanceof Error) {
+				return res.status(204).json({
+					message: error.message,
+					error: true,
+				});
+			}
+		}
+	},
+};
+
+export default juegoController;
---
+++ src/routes/juego.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import juegoController from "../controllers/juego";
+
+
+const juegoRouter = Router();
+
+//GET - http://localhost/juego
+juegoRouter.get('/', juegoController.getAll);
+
+//GET - http://localhost/juego/:id
+juegoRouter.get('/:id', juegoController.getById);
+
+//POST - http://localhost/juego
+juegoRouter.post('/', juegoController.create);
+
+//DELETE - http://localhost/juego/:id
+juegoRouter.delete('/:id', juegoController.delete);
+
+//PUT - http://localhost/juego/:id
+juegoRouter.put('/:id', juegoController.update);
+
+export default juegoRouter;
