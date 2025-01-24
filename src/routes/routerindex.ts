bd470d65d317e718474a4e64f3f2c9176d02e574
Joaquin Fernan Canto <joaquinfr.canto@gmail.com>
2025-01-24 12:09:43 -0300

    Simplified the database.

--- src/routes/routerindex.ts
+++ src/routes/routerindex.ts
@@ -2,9 +2,6 @@ import { Router } from "express";
 import juegoRouter from "./juego";
 import lugarRouter from "./lugar";
 import personaRouter from "./persona";
-import adminRouter from "./administador";
-import narradorRouter from "./narrador";
-import jugadorController from "./jugador";
 import mesaController from "./mesa";
 import inscripcionController from "./inscripcion";
 
@@ -13,9 +10,6 @@ const router = Router();
 router.use('/juego', juegoRouter);
 router.use('/lugar', lugarRouter);
 router.use('/persona', personaRouter);
-router.use('/admin', adminRouter);
-router.use('/narrador', narradorRouter);
-router.use('/jugador', jugadorController);
 router.use('/mesa', mesaController);
 router.use('/inscripcion', inscripcionController);
 
